12d838d7dde34ba31cf40391287be1f14e2ac1d4
FearHeim <narjos2000@gmail.com>
2020-08-30 16:56:58 -0500

    Validaciones

--- serve.js
+++ serve.js
@@ -5,16 +5,15 @@ const {pool}=require('./dbConfig')
 const PORT = process.env.PORT || 777;
 
 app.set("view engine", "ejs"); 
+app.use(express.urlencoded({ extended:false }));
 
 app.get('/', (req, res)=>{
     res.render('index')
 })
 
-
 app.get("/users/register", (req, res)=>{
     res.render("register");
 })
-
 
 app.get("/users/login", (req, res)=>{
     res.render("login");
@@ -23,6 +22,39 @@ app.get("/users/login", (req, res)=>{
 app.get("/users/dashboard", (req, res)=>{
     res.render("dashboard");
 })
+
+app.post('/users/register',(req, res)=>{
+    let {name, email, password, password2}=req.body;
+
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    })
+
+    let errors=[];
+
+    if(!name || !email || !password || !password2){
+        errors.push({message: "Asegurese de ingresar todos los campos"})
+
+    }
+
+    if(password.length<6){
+        errors.push({message: "La contraseña debe de tener 6 caracteres o más"})
+
+    }
+
+    if(password!==password2){
+        errors.push({message: "Las contraseñas no coinciden"})
+    }
+
+    if(errors.length>0){
+        res.render('register', {errors})
+    }
+})
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
